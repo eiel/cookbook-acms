7b627b1d166724de34daf21f1d5f9e2c33a591f1
Tomohiko Himura <eiel.hal@gmail.com>
2014-10-05 10:14:28 +0900

    installはchefで行わない

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,4 +12,4 @@ include_recipe %w{ apache2 apache2::mod_php5 apache2::mod_rewrite }
 include_recipe %w{ php php::module_mysql php::module_gd }
 
 package 'unzip'
-include_recipe %w{ acms::ioncube acms::database acms::install }
+include_recipe %w{ acms::ioncube acms::database}
